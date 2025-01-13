f4d4f65fbffbaf077b11da4f90e52894938c954f
nhutanh1120 <nhutanh1120@gmail.com>
2025-01-14 00:45:35 +0700

    20250114

--- client/src/layouts/components/Sidebar/Sidebar.js
+++ client/src/layouts/components/Sidebar/Sidebar.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import classNames from 'classnames/bind';
 import styles from './Sidebar.module.scss';
 import Menu, { MenuItem } from './Menu';
@@ -9,11 +10,21 @@ import {
     LiveIcon,
     LiveActiveIcon,
 } from '~/components/Icons';
+import SuggestedAccounts from '~/components/SuggestedAccounts';
+import * as userService from '~/services/userService';
 import config from '~/config';
 
 const cx = classNames.bind(styles);
 
 function Sidebar() {
+    const [suggestedUsers, setSuggestedUsers] = useState([]);
+
+    useEffect(() => {
+        userService.getSuggested().then((data) => {
+            setSuggestedUsers(data);
+        });
+    }, []);
+
     return (
         <aside className={cx('wrapper')}>
             <Menu>
@@ -26,6 +37,9 @@ function Sidebar() {
                 />
                 <MenuItem title="LIVE" to={config.routes.live} icon={<LiveIcon />} activeIcon={<LiveActiveIcon />} />
             </Menu>
+
+            <SuggestedAccounts label="Suggested accounts" data={suggestedUsers} />
+            <SuggestedAccounts label="Following accounts" />
         </aside>
     );
 }
